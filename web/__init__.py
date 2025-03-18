d5229f96f729ad8401641e14a26a1ac3c081a61d
CastleUser999 <idiotfriendgroup@gmail.com>
2025-03-18 13:46:07 +0800

    Update __init__.py

--- web/__init__.py
+++ web/__init__.py
@@ -11,9 +11,20 @@ class Website:
     if self.dangerous == False:
       print(f'https://{self.name}.{self.sitetype}/{self.page}')
   def debug(self):
-    ans = input("<input> <RUN PYTHON ")
+    ans = input("<input> <RUN PYTHON> ")
     exec(str(ans))
     print("</input> </RUN PYTHON>")
 class GoogleDrive:
-  os.makedirs("python package pybrowse3 googledrive", exist_ok=True)
-  
+  os.makedirs("googleDrive PyBrowse3", exist_ok=True
+  def createFile(name: str, content: str) -> None:
+    folder_path = r"C:\Users\AppData\Local\Programs\Python\Python314"
+    file_path = os.path.join(folder_path, name)
+    with open(file_path, "w") as f:
+      f.write(content) # It will Edit the file if it exists
+  def delFile(file_path):
+	  os.remove(file_path)
+	  
+      
+
+
+    
